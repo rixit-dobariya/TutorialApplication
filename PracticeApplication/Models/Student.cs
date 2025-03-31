da662b18e03a1a368ff8b8cae1096ccbc74150e4
Rixit Dobariya <rixitdobariya05@gmail.com>
2025-03-31 21:15:20 +0530

    add state management changes

---
+++ PracticeApplication/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticeApplication.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Student name must not be empty")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Student class name must not be empty")]
+        public string ClassName{ get; set; }
+        [Required(ErrorMessage = "Percentage must not be empty")]
+        [Range(0,100,ErrorMessage ="Percentage must be between 0 to 100")]
+        public double percentage { get; set; }
+    }
+}
