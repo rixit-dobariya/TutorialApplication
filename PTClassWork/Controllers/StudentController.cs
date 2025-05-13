daf45ae106ab486ed4be13e3c4bfce15f27b82e1
Rixit Dobariya <rixitdobariya05@gmail.com>
2025-05-13 07:35:33 +0530

    qwe

---
+++ PTClassWork/Controllers/StudentController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PTClassWork.Models;
+
+namespace PTClassWork.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Student().GetStudents(null));
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                if (student.Create())
+                {
+                    TempData["msg"] = "Record Added successfully!";
+                    return RedirectToAction("Index");
+
+                }
+                else
+                {
+                    TempData["msg"] = "Record insertion Failed!";
+                }
+            }
+            return View();
+        }
+        public IActionResult Edit(int id)
+        {
+                Student student = new Student().GetStudents(id).FirstOrDefault();
+                return View(student);
+        }
+        [HttpPost]
+        public IActionResult Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                if (student.Update())
+                {
+                    TempData["msg"] = "Record Updated successfully!";
+                    return RedirectToAction("Index");
+
+                }
+                else
+                {
+                    TempData["msg"] = "Record Updation Failed!";
+                }
+            }
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            Student student = new Student().GetStudents(id).FirstOrDefault() ?? new Student();
+            return View(student);
+        }
+        [HttpPost]
+        public IActionResult Delete(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                if (student.Delete())
+                {
+                    TempData["msg"] = "Record Deleted successfully!";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["msg"] = "Record Deletion Failed!";
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ PTClassWork/Models/Student.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace PTClassWork.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public int Sem { get; set; }
+        public string Branch { get; set; }
+        public string[] Hobby {get;set;}
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\01\\source\\repos\\TutorialApplication\\PTClassWork\\App_Data\\PTClassWorkDB.mdf;Integrated Security=True");
+        public bool Create()
+        {
+            bool result;
+            using (SqlCommand cmd = new SqlCommand("insert into tbl_stud2(Name, Email, Age,Sem, Branch) values(@name, @email, @age, @sem, @branch)",con))
+            {
+                cmd.Parameters.AddWithValue("name",Name);
+                cmd.Parameters.AddWithValue("email", Email);
+                cmd.Parameters.AddWithValue("age", Age);
+                cmd.Parameters.AddWithValue("sem", Sem);
+                cmd.Parameters.AddWithValue("branch", Branch);
+                con.Open();
+                result = cmd.ExecuteNonQuery()>=1;
+            }
+            con.Close();
+            return result;
+        }
+        public bool Update()
+        {
+            bool result;
+            using (SqlCommand cmd = new SqlCommand("update tbl_stud2 set Name=@name, Email=@email, Age=@age,Sem=@sem,Branch=@branch where Id=@id", con))
+            {
+                cmd.Parameters.AddWithValue("name", Name);
+                cmd.Parameters.AddWithValue("email", Email);
+                cmd.Parameters.AddWithValue("age", Age);
+                cmd.Parameters.AddWithValue("sem", Sem);
+                cmd.Parameters.AddWithValue("branch", Branch);
+                cmd.Parameters.AddWithValue("id", Id);
+                con.Open();
+                result = cmd.ExecuteNonQuery() >= 1;
+            }
+            con.Close();
+            return result;
+        }
+        public bool Delete()
+        {
+            bool result;
+            using (SqlCommand cmd = new SqlCommand("delete from tbl_stud2 where Id=@id", con))
+            {
+                cmd.Parameters.AddWithValue("id", Id);
+                con.Open();
+                result = cmd.ExecuteNonQuery() >= 1;
+            }
+            con.Close();
+            return result;
+        }
+        public List<Student> GetStudents(int? id)
+        {
+            List<Student> students = new List<Student>();
+            DataSet ds = new DataSet();
+            string query = "select * from tbl_stud2";
+            if (id!=null)
+            {
+                query = "select * from tbl_stud2 where Id = " + id;
+            }
+            using (SqlDataAdapter adapter = new SqlDataAdapter(query, con))
+            {
+                con.Open();
+                adapter.Fill(ds);
+                con.Close();
+                foreach(DataRow dr in ds.Tables[0].Rows)
+                {
+                    students.Add(new Student
+                    {
+                        Id = (int)dr["Id"],
+                        Name= (string)dr["Name"],
+                        Email = (string)dr["Email"],
+                        Age = (int)dr["Age"],
+                        Sem = (int)dr["Sem"],
+                        Branch = (string)dr["Branch"]
+                    });
+                }
+            }
+                return students;
+        }
+    }
+}
